8217cfb625dcbe5dc76d6dd78e52c00bc2c6d05e
Omar El-Sawy <ok.elsawy@gmail.com>
2025-11-14 16:11:48 -0500

    just downgraded to a version that works well with github workflows

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
   pkgs ?
     import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-      sha256 = "sha256:04h7cq8rp8815xb4zglkah4w6p2r5lqp7xanv89yxzbmnv29np2a";
+      sha256 = "sha256:18g0f8cb9m8mxnz9cf48sks0hib79b282iajl2nysyszph993yp0";
     }) {
       overlays = [
       ];
