37e0ab28edb32194e2229bff2e1f93720bcb7cc1
Omar El-Sawy <ok.elsawy@gmail.com>
2026-01-07 17:37:48 -0500

    changed base to repo name

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: '/TinyTapeout_Flows/', // Replace with your EXACT repository name
   build: {
     outDir: 'dist',
     emptyOutDir: true,
